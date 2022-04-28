91b999b08d25dd04417da1037ed48eaaef00ade0
KoonaSainath <sainathwnp@gmail.com>
2022-04-28 12:56:36 +0530

    Implemented displaying all categories list in index page

--- BulkyBook_UsingRazorPagesWeb/Pages/CategoryPages/Index.cshtml.cs
+++ BulkyBook_UsingRazorPagesWeb/Pages/CategoryPages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BulkyBook_UsingRazorPagesWeb.Data;
+using BulkyBook_UsingRazorPagesWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace BulkyBook_UsingRazorPagesWeb.Pages.CategoryPages
 {
     public class IndexModel : PageModel
     {
+        public List<Category> categories;
+        private readonly ApplicationDbContext db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
         public void OnGet()
         {
+            categories = db.Categories.ToList();
         }
     }
 }
