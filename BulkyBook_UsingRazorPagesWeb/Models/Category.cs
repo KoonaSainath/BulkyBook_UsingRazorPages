4e08993c9fc81ebf1ea2b3c97e55e91fb8997fc2
KoonaSainath <sainathwnp@gmail.com>
2022-04-28 16:50:22 +0530

    Edit and delete category functionalities implemented

--- BulkyBook_UsingRazorPagesWeb/Models/Category.cs
+++ BulkyBook_UsingRazorPagesWeb/Models/Category.cs
@@ -7,9 +7,11 @@ namespace BulkyBook_UsingRazorPagesWeb.Models
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Please enter category name")]
+        [Display(Name = "Category name")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Please enter display order")]
         [Range(1, maximum: 100, ErrorMessage = "Display order can only be from 1 to 100")]
+        [Display(Name = "Display order")]
         public int? DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
