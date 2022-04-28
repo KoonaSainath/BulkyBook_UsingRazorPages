59e66bb0bc67300741ec63565b962b42bcf5d718
KoonaSainath <sainathwnp@gmail.com>
2022-04-28 12:10:06 +0530

    Created categories index page

---
+++ BulkyBook_UsingRazorPagesWeb/Pages/CategoryPages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyBook_UsingRazorPagesWeb.Pages.CategoryPages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
